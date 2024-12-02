ada9d0bd79c7d6da651251f9b688a62d69faf49c
omar554 <62614708+omar554@users.noreply.github.com>
2024-12-02 17:58:44 -0600

    update 9

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
                 <SearchBar />
                 <HomeMenu />
             </article>
-            {/* <NewsSection /> */}
+            <NewsSection /> 
         </section>
     );
 };
